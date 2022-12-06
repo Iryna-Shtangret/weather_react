0dba80920ef82d14779ccd1c3d4a49146a9ed8cc
Iryna-Shtangret <113630738+Iryna-Shtangret@users.noreply.github.com>
2022-12-06 18:39:53 +0200

    Added Weather css and js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://sparkling-smakager-db96da.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Iryna Mrachkovska
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Iryna-Shtangret/weather_react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://sparkling-smakager-db96da.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Iryna Mrachkovska
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Iryna-Shtangret/weather_react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
