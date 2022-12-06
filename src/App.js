c53ec763a4be43a173f3a174b9dee679eaab8b18
Iryna-Shtangret <113630738+Iryna-Shtangret@users.noreply.github.com>
2022-12-06 17:50:53 +0200

    Added links to GitHub and My personsl site

--- src/App.js
+++ src/App.js
@@ -2,5 +2,28 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://sparkling-smakager-db96da.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Iryna Mrachkovska
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Iryna-Shtangret/weather_react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
